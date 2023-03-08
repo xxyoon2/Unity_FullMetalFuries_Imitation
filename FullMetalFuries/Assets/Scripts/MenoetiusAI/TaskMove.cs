b3bba4c60bccb2d20da984250781bc6af3c4a96e
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-08 21:56:26 +0900

    Feat: 노드 딜레이 구현

--- FullMetalFuries/Assets/Scripts/MenoetiusAI/TaskMove.cs
+++ FullMetalFuries/Assets/Scripts/MenoetiusAI/TaskMove.cs
@@ -6,15 +6,41 @@ using BehaviorTree;
 
 public class TaskMove : Node
 {
+    private bool _waiting = true;
+    private float _waitTime = 3f;
+    private float _waitCounter = 0f;
+
     public TaskMove()
     {
+        initialization();
+    }
 
+    private void initialization()
+    {
+        _waiting = true;
+        _waitCounter = 0f;
+        state = NodeState.FAILURE;
     }
 
     public override NodeState Evaluate()
     {
-        Debug.Log("이동");
-        state = NodeState.SUCCESS;
+        if (_waiting)
+        {
+            _waitCounter += Time.deltaTime;
+            // state = NodeState.FAILURE;
+            Debug.Log($"이동, {state}");
+
+            if (_waitCounter >= _waitTime)
+            {
+                _waiting = false;
+                state = NodeState.SUCCESS;
+            }
+        }
+        else
+        {
+            initialization();
+        }
+
         return state;
     }
 }
