9f04f2375a2c30260416e695420a91a85b11527f
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-14 15:57:23 +0900

    feat: 도약 공격 구현

--- FullMetalFuries/Assets/Scripts/FSM/MenoeJump.cs
+++ FullMetalFuries/Assets/Scripts/FSM/MenoeJump.cs
@@ -13,14 +13,13 @@ public class MenoeJump : StateMachineBehaviour
 
     private float _counter = 0f;
 
-    private const float JUMP_TIME = 1f;
-    private const float JUMP_HEIGHT = 10f;
+    private const float JUMP_HEIGHT = 20f;
     private const float SPEED = 5f;
 
     private Vector2 BezierCurve(float t)
     {
         float s = 1 - t;
-        return (Mathf.Pow(s, 3) * _jumpingPoint) + (3 * Mathf.Pow(s, 2) * t * _point1) + (3 * s * Mathf.Pow(t, 2) * _point2) + (Mathf.Pow(t, 3) * _targetPoint);
+        return (Mathf.Pow(s, 2) * _jumpingPoint) + (2 * s * t * _point) + (Mathf.Pow(t, 2) * _targetPoint);
     }
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -29,8 +28,7 @@ public class MenoeJump : StateMachineBehaviour
         _jumpingPoint = _transform.position;
         _targetPoint = GameObject.FindWithTag("Player").transform.position;
 
-        _point1 = new Vector2(_jumpingPoint.x, _jumpingPoint.y + JUMP_HEIGHT);
-        _point2 = new Vector2(_targetPoint.x, _targetPoint.y + JUMP_HEIGHT);
+        _point = new Vector2(_jumpingPoint.x, _jumpingPoint.y + JUMP_HEIGHT);
 
         _counter = 0f;
         animator.SetBool("isStop", false);
@@ -38,13 +36,29 @@ public class MenoeJump : StateMachineBehaviour
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (_counter >= JUMP_TIME)
+        float distance = Vector3.Distance(_transform.position, _targetPoint);
+        if (distance <= 0.1)
         {
-            _transform.position = _jumpingPoint;
+            _transform.position = _targetPoint;
             animator.SetBool("isStop", true);
         }
-        _counter += Time.deltaTime;
-        Vector2 movePoint = BezierCurve(_counter);
-        _transform.position = Vector2.MoveTowards(movePoint, movePoint, SPEED * Time.deltaTime);
+        else
+        {
+            _counter += Time.deltaTime;
+            Vector2 movePoint = BezierCurve(_counter);
+            _transform.position = Vector2.MoveTowards(movePoint, movePoint, SPEED * Time.deltaTime);
+        }
+    }
+
+    private const float ATTACK_RANGE_RADIUS = 3f;
+    private const int PLAYER_LAYER = 1 << 7;
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform attackRange = animator.transform.Find("RecoilRange");
+        Collider2D collider = Physics2D.OverlapCircle(attackRange.position, ATTACK_RANGE_RADIUS, PLAYER_LAYER);
+        if (collider != null)
+        {
+            // 데미지 입혔다고 알리기
+        }
     }
 }
