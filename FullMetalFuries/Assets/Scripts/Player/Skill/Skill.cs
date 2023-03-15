668a3e966b4a2bed093dabd6b1c75fa7a87baf07
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-15 15:55:24 +0900

    Chore: 스킬 클래스 추가

---
+++ FullMetalFuries/Assets/Scripts/Player/Skill/Skill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Skill
+{
+    public int _damage;
+
+    public Skill()
+    {
+        _damage = 1;
+    }
+
+    public Skill(int damage)
+    {
+        _damage = damage;
+    }
+}
