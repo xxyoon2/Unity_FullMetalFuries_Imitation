903f863742dfb038d790633cfa37b375bac378ec
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-18 18:27:48 +0900

    feat: 보스 체력바 구현

---
+++ FullMetalFuries/Assets/Scripts/UI/Battle/BattleView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleView : View
+{
+    public Slider bossHealthBar { get; private set; }
+
+    void Awake()
+    {
+        bossHealthBar = RectTransform.FindObjectOfType<Slider>();
+        Debug.Assert(bossHealthBar != null, "체력바가 들어오지 않았습니다.");
+    }
+}
---
+++ FullMetalFuries/Assets/Scripts/UI/Battle/BattleViewPresenter.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+public class BattleViewPresenter : Presenter
+{
+    private BattleView _battleView;
+    private CompositeDisposable _compositeDisposable = new CompositeDisposable();
+
+    public override void OnInitialize(View view)
+    {
+        _battleView = view as BattleView;
+        Model.BattleModel.InitHP();
+        InitializeRx();
+    }
+
+    /// <summary>
+    /// ViewController가 파괴될 때 호출
+    /// 자원 정리 용도
+    /// </summary>
+    public override void OnRelease()
+    {
+        _battleView = default;
+        _compositeDisposable.Dispose();
+    }
+
+    /// <summary>
+    /// View에 유저 이벤트가 발생했을 때 동작
+    /// 보통 Model을 업데이트 
+    /// </summary>
+    protected override void OnOccuredUserEvent()
+    {
+
+    }
+
+    /// <summary>
+    /// Model이 업데이트 되었을 때 동작
+    /// 보통 View를 업데이트 
+    /// </summary>
+    protected override void OnUpdateModel()
+    {
+        Model.BattleModel.hpData.Subscribe(UpdateBossHealthBar).AddTo(_compositeDisposable);
+    }
+
+    private void UpdateBossHealthBar(int hp)
+    {
+        _battleView.bossHealthBar.value = hp;
+    }
+
+    //public void Oninitialize()
+    //{
+    //    _battleView = 
+    //}
+
+    //public void OnRelease()
+    //{
+    //    _battleView = default;
+    //    _compositeDisposable.Dispose();
+    //}
+
+    //private void InitalizeRx()
+    //{
+    //    
+    //}
+
+    //private void OnHpBar(int hp)
+    //{
+    //   
+    //}
+
+    //private void OnClose()
+    //{
+
+    //}
+}
