468347ed1040963ce443754775e523a91af632de
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-21 08:21:24 +0900

    chore: 일반 공격 움직임 구현부 수정

--- FullMetalFuries/Assets/Scripts/Player/PlayerAttack.cs
+++ FullMetalFuries/Assets/Scripts/Player/PlayerAttack.cs
@@ -8,6 +8,9 @@ public class PlayerAttack : StateMachineBehaviour
     private Rigidbody2D _rigidbody;
     private Controller _controller;
 
+    private float _x;
+    private float _y;
+
     private const float MOVE_SPEED = 1f;
     private const int DAMAGE = 5;
     private const int ENEMY_LAYER = 1 << 8;
@@ -17,6 +20,18 @@ public class PlayerAttack : StateMachineBehaviour
         _transform = animator.transform;
         _rigidbody = _transform.GetComponent<Rigidbody2D>();
         _controller = _transform.GetComponent<Controller>();
+
+        _x = _controller.x;
+        _y = _controller.y;
+
+        if (_controller.x > 0)
+        {
+            _transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            _transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
 
         Transform attackRange = animator.transform.Find("AttackRange");
         Collider2D collider = Physics2D.OverlapBox(attackRange.transform.position, attackRange.transform.localScale, 0f, ENEMY_LAYER);
@@ -34,16 +49,7 @@ public class PlayerAttack : StateMachineBehaviour
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Vector2 playerPosition = _transform.position;
-        Vector2 point = playerPosition + (Vector2.right * _controller.x + Vector2.up * _controller.y) * MOVE_SPEED * Time.fixedDeltaTime;
+        Vector2 point = playerPosition + (Vector2.right * _x + Vector2.up * _y) * MOVE_SPEED * Time.fixedDeltaTime;
         _rigidbody.MovePosition(point);
-
-        if (_controller.x > 0)
-        {
-            _transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        else
-        {
-            _transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
     }
 }
