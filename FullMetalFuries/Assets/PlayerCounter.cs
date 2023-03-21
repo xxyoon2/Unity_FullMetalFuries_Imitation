a87cfc1d50ee195306bc9cb1f50b74aa06f21a83
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2023-03-21 11:18:22 +0900

    chore: 주석 추가

--- FullMetalFuries/Assets/PlayerCounter.cs
+++ FullMetalFuries/Assets/PlayerCounter.cs
@@ -26,6 +26,7 @@ public class PlayerCounter : StateMachineBehaviour
             Vector2 playerPosition = _transform.position;
             Vector2 point = playerPosition + (Vector2.right * _controller.x + Vector2.up * _controller.y) * MOVE_SPEED * Time.fixedDeltaTime;
             _rigidbody.MovePosition(point);
+            // 데미지도 줘야함 
         }
     }
 
@@ -34,26 +35,4 @@ public class PlayerCounter : StateMachineBehaviour
         _transform.GetComponent<PlayerBehavior>().SetState(PlayerBehavior.State.NONE);
         animator.SetBool("isCounterStrike", false);
     }
-
-    // OnStateMove is called before OnStateMove is called on any state inside this state machine
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateIK is called before OnStateIK is called on any state inside this state machine
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    //override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
-    //{
-        
-    //}
-
-    //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
-    //{
-        
-    //}
 }
